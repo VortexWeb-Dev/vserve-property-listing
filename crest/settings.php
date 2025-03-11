b95d1a16011185e5bd3a96135bb7d54a5c34e00a
Muhammed F <muhammedfasilofficial@gmail.com>
2025-03-11 18:33:29 +0530

    Update settings.php

--- crest/settings.php
+++ crest/settings.php
@@ -1,7 +1,7 @@
 <?php
 define('WEB_HOOK_URL', 'https://vserve.bitrix24.in/rest/10/a13qt5v1wx2q2x64/');
-define('C_REST_CLIENT_ID', 'local.67a42e7604be80.81184164');
-define('C_REST_CLIENT_SECRET', 'qNtQtuPsoke91xxeS2d45AF311Mlf5MdTHJ9zM0uQoiURPwOZt');
+define('C_REST_CLIENT_ID', 'local.67d0341c407f74.25967800');
+define('C_REST_CLIENT_SECRET', '8ubLbmDEmlHaZhnXMwZ39Sd5CTvAquSG6xeo2s0VIpiABtnOV9');
 
 define('DEVELOPERS_ENTITY_TYPE_ID', 1044);
 define('AGENTS_ENTITY_TYPE_ID', 1052);
