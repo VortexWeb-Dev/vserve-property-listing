61c9c78878b95a19ea41c547866822b24d66e9d0
ajzal-byte <ajzalaju10@gmail.com>
2025-04-04 16:23:15 +0530

    watermark updated and size changed

--- views/footer.php
+++ views/footer.php
@@ -487,10 +487,10 @@
                 let watermarkWidth, watermarkHeight;
 
                 if (watermarkAspect > imageAspect) {
-                    watermarkWidth = width * 0.6;
+                    watermarkWidth = width * 0.3;
                     watermarkHeight = watermarkWidth / watermarkAspect;
                 } else {
-                    watermarkHeight = height * 0.6;
+                    watermarkHeight = height * 0.3;
                     watermarkWidth = watermarkHeight * watermarkAspect;
                 }
 
